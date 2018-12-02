700d1a266d5b702e2b02766dd8d5803ea3c67622
Elise Rubio <erubio@enseirb-matmeca.fr>
2018-12-02 18:18:51 +0100

    Added getUsers

--- app/src/main/java/enseirb/projetapplicationsportive/Database.java
+++ app/src/main/java/enseirb/projetapplicationsportive/Database.java
@@ -2,7 +2,11 @@ package enseirb.projetapplicationsportive;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Database {
     private final static int VERSION_DATABASE = 1;
@@ -27,6 +31,22 @@ public class Database {
         values.put(SQLiteBase.USER_NAME, name);
 
         return database.insert(SQLiteBase.USERS_TABLE, null, values);
+    }
+
+    public List<String> getUsers(){
+        String[] columns = {SQLiteBase.USER_NAME};
+        String order_by = SQLiteBase.USER_NAME + " ASC";
+        Cursor cursor = database.query(SQLiteBase.USERS_TABLE, columns, null, null, null,
+                null, order_by, null);
+
+        List<String> users = new ArrayList<>();
+
+        while(!cursor.isAfterLast()){
+            users.add(cursor.getString(0));
+            cursor.moveToNext();
+        }
+
+        return users;
     }
 
     /*public long insertRun(Run run){
