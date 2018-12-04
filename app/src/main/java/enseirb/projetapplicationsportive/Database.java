fca761749ed8b0fa3a90499e065f2fdbf734426d
Elise Rubio <erubio@enseirb-matmeca.fr>
2018-12-04 14:42:11 +0100

    deleteUser done
    You can now delete a user and all their runs!

--- app/src/main/java/enseirb/projetapplicationsportive/Database.java
+++ app/src/main/java/enseirb/projetapplicationsportive/Database.java
@@ -32,58 +32,61 @@ public class Database {
         mySqliteBase.close();
     }
 
-    public Run getLastRun(long runner_id){
-        List<Run> runs = getRuns(runner_id);
+    public Run getLastRun(long runnerId){
+        List<Run> runs = getRuns(runnerId);
         return runs.get(0);
     }
 
-    public List<Run> getRuns(long runner_id){
-        String[] columns_runs_query = {SQLiteBase.RUN_ID};
-        String[] columns_entries_query = {SQLiteBase.ENTRY_TIME, SQLiteBase.ENTRY_LATITUDE, SQLiteBase.ENTRY_LONGITUDE};
-        String[] columns_runners_query = {SQLiteBase.USER_NAME};
+    public List<Run> getRuns(long runnerId){
+        String[] columnsRunsQuery = {SQLiteBase.RUN_ID};
+        String[] columnsEntriesQuery = {SQLiteBase.ENTRY_TIME, SQLiteBase.ENTRY_LATITUDE, SQLiteBase.ENTRY_LONGITUDE};
+        String[] columnsRunnersQuery = {SQLiteBase.USER_NAME};
 
-        String selection_runs_query = SQLiteBase.RUN_RUNNER_ID + " = " + runner_id;
-        String selection_runners_query = SQLiteBase.USER_ID + " = " + runner_id;
+        String selectionRunsQuery = SQLiteBase.RUN_RUNNER_ID + " = " + runnerId;
+        String selectionRunnersQuery = SQLiteBase.USER_ID + " = " + runnerId;
 
-        String order_by_runs_query = SQLiteBase.RUN_ID + " DESC";
-        String order_by_entries_query = SQLiteBase.ENTRY_TIME + " DESC";
+        String orderByRunsQuery = SQLiteBase.RUN_ID + " DESC";
+        String orderByEntriesQuery = SQLiteBase.ENTRY_TIME + " DESC";
 
         // First retrieving all the runs
 
-        Cursor cursor_runs = database.query(true, SQLiteBase.RUNS_TABLE, columns_runs_query , selection_runs_query,
-                null, null, null, order_by_runs_query, null);
+        Cursor cursorRuns = database.query(true, SQLiteBase.RUNS_TABLE, columnsRunsQuery , selectionRunsQuery,
+                null, null, null, orderByRunsQuery, null);
+        cursorRuns.moveToFirst();
 
         // For each run, retrieving its entries
         List<Run> runs = new ArrayList<Run>();
 
-        while(!cursor_runs.isAfterLast()){
+        while(!cursorRuns.isAfterLast()){
             Map<Date, Location> entries = new HashMap<Date, Location>();
 
-            String selection_entries_query = SQLiteBase.ENTRY_RUN_ID + " = " + cursor_runs.getString(0);
-            Cursor cursor_entries = database.query(true, SQLiteBase.ENTRIES_TABLE, columns_entries_query, selection_entries_query,
-                    null, null, null, order_by_entries_query, null);
+            String selectionEntriesQuery = SQLiteBase.ENTRY_RUN_ID + " = " + cursorRuns.getString(0);
+            Cursor cursorEntries = database.query(true, SQLiteBase.ENTRIES_TABLE, columnsEntriesQuery, selectionEntriesQuery,
+                    null, null, null, orderByEntriesQuery, null);
+            cursorEntries.moveToFirst();
 
-            while(!cursor_entries.isAfterLast()){
-
+            while(!cursorEntries.isAfterLast()){
 
                 try {
-                    Date date = new SimpleDateFormat(DATE_PATTERN).parse(cursor_entries.getString(0));
+                    Date date = new SimpleDateFormat(DATE_PATTERN).parse(cursorEntries.getString(0));
                     // TODO: Retrieve the location!!
                 } catch(ParseException e){
                     e.printStackTrace();
                 }
 
-                cursor_entries.moveToNext();
+                cursorEntries.moveToNext();
             }
 
-            Cursor cursor_runners = database.query(true, SQLiteBase.USERS_TABLE, columns_runners_query, selection_runners_query,
+            Cursor cursorRunners = database.query(true, SQLiteBase.USERS_TABLE, columnsRunnersQuery, selectionRunnersQuery,
                     null, null, null, null, null);
 
-            String runner_name = cursor_runners.getString(0);
+            cursorRunners.moveToFirst();
 
-            runs.add(new Run(entries, runner_name, runner_id));
+            String runnerName = cursorRunners.getString(0);
 
-            cursor_runs.moveToNext();
+            runs.add(new Run(entries, runnerName, runnerId));
+
+            cursorRuns.moveToNext();
         }
 
         return runs;
@@ -102,21 +105,21 @@ public class Database {
 
         values.put(SQLiteBase.RUN_RUNNER_ID, run.getRunnerId());
 
-        long run_id = database.insert("runs", null, values);
+        long runId = database.insert("runs", null, values);
 
         Map<Date, Location> path = run.getPath();
 
         for (Date date: path.keySet()) {
-            insertEntry(run_id, date, path.get(date));
+            insertEntry(runId, date, path.get(date));
         }
 
-        return run_id;
+        return runId;
     }
 
-    private long insertEntry(long run_id, Date date, Location location){
+    private long insertEntry(long runId, Date date, Location location){
         ContentValues values = new ContentValues();
 
-        values.put(SQLiteBase.ENTRY_RUN_ID, run_id);
+        values.put(SQLiteBase.ENTRY_RUN_ID, runId);
 
         String formatted_date = new SimpleDateFormat(DATE_PATTERN).format(date);
         values.put(SQLiteBase.ENTRY_TIME, formatted_date);
@@ -128,19 +131,34 @@ public class Database {
         return database.insert(SQLiteBase.ENTRIES_TABLE, null, values);
     }
 
-    public int deleteUser(String name){
-//        deleteRuns()
+    public int deleteUser(String name, long userId){
+        deleteRuns(userId);
         return database.delete(SQLiteBase.USERS_TABLE, SQLiteBase.USER_NAME + " = " + name, null);
     }
 
-    public int deleteRuns(long user_id){
-//TODO: implement this
-        return 0;
+    public int deleteRuns(long userId){
+        int res = 0;
+
+        // Look for all the user's runs
+        String[] columns = {SQLiteBase.RUN_ID};
+        String selection = SQLiteBase.RUN_RUNNER_ID + " = " + userId;
+
+        Cursor cursorRuns = database.query(true, SQLiteBase.RUNS_TABLE, columns, selection,
+                null, null, null, null, null);
+        cursorRuns.moveToFirst();
+
+        // For each run, delete its entries and then delete the run
+        while(!cursorRuns.isAfterLast()) {
+            long runId = Long.parseLong(cursorRuns.getString(0));
+            deleteEntries(runId);
+            res += database.delete(SQLiteBase.RUNS_TABLE, SQLiteBase.RUN_ID + " = " + runId, null);
+            cursorRuns.moveToNext();
+        }
+
+        return res;
     }
 
-    public int deleteEntries(long run_id){
-//TODO: implement this
-
-        return 0;
+    private int deleteEntries(long runId){
+        return database.delete(SQLiteBase.ENTRIES_TABLE, SQLiteBase.ENTRY_RUN_ID + " = " + runId, null);
     }
 }
