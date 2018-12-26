6849dc065d54357d14ffd55858a2e4464245abee
Lysoun <lisouni2@gmail.com>
2018-12-26 19:23:48 +0100

    Added a listView to display the runs in history

--- app/src/main/java/enseirb/projetapplicationsportive/HistoryActivity.java
+++ app/src/main/java/enseirb/projetapplicationsportive/HistoryActivity.java
@@ -4,19 +4,48 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class HistoryActivity extends AppCompatActivity {
+    private ListView runsListView;
+    private Database database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
         long userId = this.getIntent().getLongExtra("userId", -1);
+
+        database = new Database(this);
+        database.open();
+
+        runsListView = (ListView) findViewById(R.id.hist_listview);
+        List<Run> runs = database.getRuns(userId);
+
+        String[] runsList = new String[runs.size()];
+
+        for(int i = 0; i < runs.size(); i++){
+            runsList[i] = runs.get(i).toString();
+        }
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_run_list_view_item, R.id.listview_tv, runsList);
+        runsListView.setAdapter(arrayAdapter);
+
     }
 
     public void goToStart(View view){
         Intent intent = new Intent(this, StartActivity.class);
         intent.putExtra("userId", this.getIntent().getLongExtra("userId", -1));
         startActivity(intent);
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        database.close();
     }
 }
