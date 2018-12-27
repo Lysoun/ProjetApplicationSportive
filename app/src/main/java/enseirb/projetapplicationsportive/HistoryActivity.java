88ab6b8463da4cde1ca4066a97808b6a0f2c6c0a
Helene <hel.bal.let@gmail.com>
2018-12-27 01:16:50 +0100

    Merge done, history prints runs. Issue : two runs stored.
    After one run was done, 2 runs are stored (one fake and one with one position in my case). Still needs to work on presentation of the runs.

--- app/src/main/java/enseirb/projetapplicationsportive/HistoryActivity.java
+++ app/src/main/java/enseirb/projetapplicationsportive/HistoryActivity.java
@@ -3,6 +3,7 @@ package enseirb.projetapplicationsportive;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -23,18 +24,27 @@ public class HistoryActivity extends AppCompatActivity {
         database = new Database(this);
         database.open();
 
+        Log.i("GpsThread", "HistoryActivity onCreate()");
+
         runsListView = (ListView) findViewById(R.id.hist_listview);
         List<Run> runs = database.getRuns(userId);
 
+        Log.i("GpsThread", "HistoryActivity getRuns");
+
         String[] runsList = new String[runs.size()];
+
+        Log.i("GpsThread", "HistoryActivity runsList");
 
         for(int i = 0; i < runs.size(); i++){
             runsList[i] = runs.get(i).toString();
         }
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_run_list_view_item, R.id.listview_tv, runsList);
+        Log.i("GpsThread", "HistoryActivity toString");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_run_list_view_item, R.id.run_listview_tv, runsList);
         runsListView.setAdapter(arrayAdapter);
 
+        Log.i("GpsThread", "HistoryActivity listView done");
     }
 
     public void goToStart(View view){
